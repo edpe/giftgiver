023d352ac28aa8553d8af089bd76e9e9b8c54588
Ed Perkins <ed@edperkins.co.uk>
2018-05-02 09:40:44 +0100

    setup for Enzyme

---
+++ src/setupTests.js
@@ -0,0 +1,5 @@
+import requestAnimationFrame from './tempPollyfills';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter(), disableLifecycleMethods: true });
