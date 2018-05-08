5c2adf7ec248bc933b6c880016013790dfeeb2f1
Ed Perkins <ed@edperkins.co.uk>
2018-05-08 11:03:31 +0100

    add gift to gift list

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -21,5 +21,5 @@ it ('adds a new gift to `state` when clicking the `add gift` button', () => {
 it( 'adds a new gift to the rendered list when clicking the `add gift` button', () => {
   app.find('.btn-add').simulate('click');
 
-  expect(app.find('.gift-list').children().length).toEqual(1);
+  expect(app.find('.gift-list').children().length).toEqual(2);
 });
