26a101c7c3ff0a8e50426940b69e280d1dba9a03
Ed Perkins <ed@edperkins.co.uk>
2018-05-08 15:30:08 +0100

    add styling

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -10,7 +10,7 @@ class Gift extends Component {
 
   render() {
     return (
-      <div>
+      <div className='gift'>
         <Form>
           <FormGroup>
             <ControlLabel>Person</ControlLabel>
