364bdb6e182d1b666b51ce10743eae8f03252bd8
Ed Perkins <ed@edperkins.co.uk>
2018-05-08 13:22:37 +0100

    add present to gift

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -19,6 +19,13 @@ class Gift extends Component {
               onChange={ event => this.setState({ person: event.target.value })}
               />
             </FormGroup>
+            <FormGroup>
+              <ControlLabel>Present</ControlLabel>
+                <FormControl
+                className='input-present'
+                onChange={ event => this.setState({ present: event.target.value })}
+                />
+              </FormGroup>
           </Form>
       </div>
     );
