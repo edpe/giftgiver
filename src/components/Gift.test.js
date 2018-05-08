24cdc60003ef742cdd16de9e8e037c4ce3b7d961
Ed Perkins <ed@edperkins.co.uk>
2018-05-08 11:40:57 +0100

    gift initialises with blank person and present

--- src/components/Gift.test.js
+++ src/components/Gift.test.js
@@ -4,8 +4,12 @@ import Gift from './Gift';
 
 describe('Gift', () => {
   const gift = shallow(<Gift />);
-  
+
   it('renders properly', () => {
     expect(gift).toMatchSnapshot();
+  });
+
+  it('initialises a person and present in `state`', () => {
+    expect(gift.state()).toEqual({ person: '', present: ''});
   });
 });
