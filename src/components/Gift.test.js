40b215d9430da553b9ac28de00b0e4c929c1bdec
Ed Perkins <ed@edperkins.co.uk>
2018-05-08 11:53:08 +0100

    test input person name

--- src/components/Gift.test.js
+++ src/components/Gift.test.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Gift from './Gift';
+import React from "react";
+import { shallow } from "enzyme";
+import Gift from "./Gift";
 
-describe('Gift', () => {
+describe("Gift", () => {
   const gift = shallow(<Gift />);
 
-  it('renders properly', () => {
+  it("renders properly", () => {
     expect(gift).toMatchSnapshot();
   });
 
-  it('initialises a person and present in `state`', () => {
-    expect(gift.state()).toEqual({ person: '', present: ''});
+  it("initialises a person and present in `state`", () => {
+    expect(gift.state()).toEqual({ person: "", present: "" });
+  });
+
+  describe("when typing into the person input", () => {
+    const person = 'Bob'
+    beforeEach(() => {
+      gift
+        .find(".input-person")
+        .simulate("change", { target: { value: person } });
+    });
+
+    it("updates the person in `state`", () => {
+      expect(gift.state().person).toEqual(person);
+    });
   });
 });
